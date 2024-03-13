dc15969c99b8bb76372ed4504c92d5be313061c7
João Souto <joaosouto0@outlook.com>
2024-03-13 10:13:45 -0300

    update

---
+++ TrocaVariaveis.java
@@ -0,0 +1,17 @@
+public class TrocaVariaveis{
+    public static void main(String[] args){
+        int numero1 = 3;        
+        int numero2 = 9;        
+        int auxiliar;  
+
+        System.out.println("Número 1 é " + numero1 + ".");
+        System.out.println("Número 2 é " + numero2 + ".");
+
+        auxiliar = numero1;
+        numero1 = numero2;      
+        numero2 = auxiliar;
+
+        System.out.println("Após a troca, número 1 passa a ser " + numero1 + ".");      
+        System.out.println("Após a troca, número 2 passa a ser " + numero2 + ".");      
+    }
+}
---
+++ exercicios1/AreaRetangulo.java
@@ -0,0 +1,8 @@
+public class AreaRetangulo{
+    public static void main(String[] args){
+        int base = 10;
+        int altura = 5;
+        int area = base * altura;
+        System.out.println("A área do retângulo é " + area + ".");
+    }
+}

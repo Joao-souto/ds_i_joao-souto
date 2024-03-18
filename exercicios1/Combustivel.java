583531c72ea6bc0c5a7312936413b6b28fe31457
João Souto <joaosouto0@outlook.com>
2024-03-18 08:53:16 -0300

    combustivel

--- exercicios1/Combustivel.java
+++ exercicios1/Combustivel.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+// importação de biblioteca para utilizar "imput"
 public class Combustivel{
     public static void main(String[] args){
+        
+        Scanner escanear = new Scanner(System.in);
+        // atribuindo método de scanner a uma variável
+
+        System.out.println("Digite a quantidade de Km/l: ");
+        float kml = escanear.nextFloat();
+
+        System.out.println("Digite a distância a ser percorrida(em km): ");
+        float distancia = escanear.nextFloat(); 
+
+        System.out.println("Digite o preço unitário do litro do combustível: ");
+        float precoLitro = escanear.nextFloat(); 
+
+        float litrosCombustivel = distancia / kml;
+        float precoFinal = litrosCombustivel * precoLitro;
+
         
     }
 }

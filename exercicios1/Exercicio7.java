21ef82d0c63cc4a901aac6d50b7342f1a6242e45
João Souto <joaosouto0@outlook.com>
2024-03-20 10:18:43 -0300

    update

---
+++ exercicios1/Exercicio7.java
@@ -0,0 +1,19 @@
+package exercicios1;
+import java.util.Scanner;
+
+public class Exercicio7 {
+    public static void main (String[] args){
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Insira o valor do seu salário: ");
+        float salario = leia.nextFloat();
+
+        System.out.println("Insira o percentual de reajuste: ");
+        float porcentagemReajuste = leia.nextFloat();
+
+        float salarioAjustado = (salario * porcentagemReajuste / 100) + salario;
+
+        System.out.println("Seu salário após o reajuste passa a ser " + salarioAjustado + " reais.");
+    }
+}

37c2de526c181bab15e923366fd6367cd7fcf878
João Souto <joaosouto0@outlook.com>
2024-03-20 10:33:00 -0300

    update

---
+++ exercicios1/Exercicio8.java
@@ -0,0 +1,15 @@
+package exercicios1;
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args){
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Insira o custo de fábrica do seu carro: ");
+        float precoFabrica = leia.nextFloat();
+
+        float precoFinal = 
+
+    }
+}

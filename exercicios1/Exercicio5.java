0815b2ba829c547a18484a425cbac9dc53cf1257
João Souto <joaosouto0@outlook.com>
2024-03-20 09:27:06 -0300

    update

--- exercicios1/Exercicio5.java
+++ exercicios1/Exercicio5.java
@@ -1,18 +1,17 @@
 package exercicios1;
 import java.util.Scanner;
-
 public class Exercicio5 {
-    public static void main(String args){
+    public static void main(String[] args){
 
         Scanner leia = new Scanner(System.in);
 
         System.out.println("Insira sua idade: ");
-        float idade = leia.nextFloat();
+        float idade = leia.nextInt();
 
         float dias = idade * 365;
         float meses = dias / 30;
 
-        System.out.println("Você têm " + dias + " e " + meses + " de vida.");
+        System.out.println("Você têm " + dias + "dias de vida e " + meses + " meses de vida.");
 
     }
 }

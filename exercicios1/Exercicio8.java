2ba388b03730f32b2460bcdcabce03fea4bc20c3
João Souto <joaosouto0@outlook.com>
2024-03-21 08:06:41 -0300

    update

--- exercicios1/Exercicio8.java
+++ exercicios1/Exercicio8.java
@@ -6,10 +6,12 @@ public class Exercicio8 {
 
         Scanner leia = new Scanner(System.in);
 
-        System.out.println("Insira o custo de fábrica do seu carro: ");
+        System.out.println("Insira o custo de fábrica do seu carro(em reais): ");
         float precoFabrica = leia.nextFloat();
 
-        float precoFinal = 
+        float precoFinal = precoFabrica + (28 * precoFabrica  / 100) + (45 * precoFabrica / 100);
+
+        System.out.println("Considerando o custo percentual do distribuidor e dos impostos o valor final deste carro é " + precoFinal + " reais.");
 
     }
-}
+} 

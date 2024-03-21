c4fb65c50340c7998786f1bebb8a9fdffd8a1f7d
João Souto <joaosouto0@outlook.com>
2024-03-21 08:40:50 -0300

    update

--- exercicios1/Exercicio10.java
+++ exercicios1/Exercicio10.java
@@ -1,5 +1,13 @@
 package exercicios1;
+import java.util.Scanner;
 
 public class Exercicio10 {
-    
+    public static void main (String[] args){
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Insira seu nome: ");
+        String nome = leia.nextLine();
+
+        leia.close();
+    }
 }

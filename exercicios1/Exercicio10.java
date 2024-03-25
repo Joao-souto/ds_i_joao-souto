c145b3ab286cede64e49e589098ba36dcb2947ef
João Pedro <joaosouto0@outlook.com>
2024-03-25 07:42:35 -0300

    update

--- exercicios1/Exercicio10.java
+++ exercicios1/Exercicio10.java
@@ -8,6 +8,29 @@ public class Exercicio10 {
         System.out.println("Insira seu nome: ");
         String nome = leia.nextLine();
 
+        System.out.println("Insira sua altura: ");
+        float altura = leia.nextFloat();
+
+        System.out.println("Insira seu sexo(M ou F): ");
+        String sexo = leia.nextLine();
+        sexo = sexo.toLowerCase();
+
+        if(sexo == "m" || sexo == "masculino" ){
+            float pesoIdeal = (72.7 * altura) - 58;
+
+            System.out.println("Olá, " + nome + "! Seu peso ideal é " + pesoIdeal + " Kg.");
+        }
+
+        else if(sexo == "f" || sexo == "feminino" ){
+            float pesoIdeal = (62.1 * altura) - 44.7;
+
+            System.out.println("Olá, " + nome + "! Seu peso ideal é " + pesoIdeal + " Kg.");
+        }
+
+        else{
+            System.out.println("Insira seu sexo correto!");
+        }
+
         leia.close();
     }
 }

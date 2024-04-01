c0ecabecb662fa45b65faf90234887dedd86d0e0
João Souto <joaosouto0@outlook.com>
2024-04-01 09:13:47 -0300

    Switch Case

---
+++ exercicios2/SwitchCase3.java
@@ -0,0 +1,15 @@
+package exercicios2;
+
+public class SwitchCase3 {
+    
+}
+/*3.   Faça um programa que receba dois números e execute as operações listadas a seguir de acordo com a escolha do usuário:
+
+Escolha do usuário	Operação
+1	Média entre os números digitados
+2	Diferença entre os números
+3	Produtos entre os números digitados
+4	Divisão do primeiro pelo segundo
+
+Se a opção digitada for inválida, mostrar uma mensagem de erro e terminar a execução do programa.
+*/

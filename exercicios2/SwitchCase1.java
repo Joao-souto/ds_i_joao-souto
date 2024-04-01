a3225d1d0067e6af5664e704883bf7543c506383
João Souto <joaosouto0@outlook.com>
2024-04-01 08:06:19 -0300

    Switch Case

--- exercicios2/SwitchCase1.java
+++ exercicios2/SwitchCase1.java
@@ -1,8 +1,49 @@
 package exercicios2;
 import java.util.Scanner;
-
 public class SwitchCase1 {
     public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Insira o código do produto: ");
+        int codProduto = leia.nextInt();
+
+        switch(codProduto){
+            case 1:
+                System.out.println("Alimento não-perecível");
+            break; 
+
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("Alimento perecível");
+            break; 
+
+            case 5:
+            case 6:
+                System.out.println("Vestuário");
+            break; 
+
+            case 7:
+                System.out.println("Higiene pessoal");
+            break; 
+
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+                System.out.println("Limpeza e Utensílios Domésticos");
+            break;  
+
+            default:
+                System.out.println("Código inválido!");
+            break;
+        }
+
+        leia.close();
 
     }
 }
---
+++ exercicios2/SwitchCase2.java
@@ -0,0 +1,33 @@
+package exercicios2;
+import java.util.Scanner;
+
+public class SwitchCase2 {
+    public static void main (String[] args){
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Insira sua primeira nota: ");
+        int nota1 = leia.nextInt();
+
+        System.out.println("Insira sua segunda nota: ");
+        int nota2 = leia.nextInt();
+
+        int media = (nota1+nota2)/2;
+
+        //EM CASO ONDE SE TRABALHA COM FLOAT, O IDEAL É USAR O "IF"
+
+        if (media>=0 && media<=4) {
+            System.out.println("Sua média é " + media + ". Você foi reprovado!");
+        }
+        else if(media>4 && media<=7){
+            System.out.println("Sua média é " + media + ". Você fará um exame!");
+        }
+        else if(media>7 && media<=10){
+            System.out.println("Sua média é " + media + ". Você foi aprovado!");
+        }
+        else{
+            System.out.println("Insira valores válidos!");
+        }
+
+        leia.close();
+    }
+}

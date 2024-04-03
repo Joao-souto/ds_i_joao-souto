339024cbb35173efa4289869ea96ecb04ad39bcf
João Souto <joaosouto0@outlook.com>
2024-04-03 09:07:58 -0300

    Switch Case

--- exercicios2/SwitchCase3.java
+++ exercicios2/SwitchCase3.java
@@ -1,15 +1,19 @@
 package exercicios2;
+import java.util.Scanner;
 
 public class SwitchCase3 {
-    
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Insira o número 1: ");
+        float num1 = leia.nextFloat();
+
+        System.out.println("Insira o número 2: ");
+        float num2 = leia.nextFloat();
+
+        System.out.println("Insira a operação desejada: \n1- Média\n2- Diferença\n3- Produto\n4- Divisão");
+        float operacao = leia.nextInt();
+
+        leia.close();
+    }
 }
-/*3.   Faça um programa que receba dois números e execute as operações listadas a seguir de acordo com a escolha do usuário:
-
-Escolha do usuário	Operação
-1	Média entre os números digitados
-2	Diferença entre os números
-3	Produtos entre os números digitados
-4	Divisão do primeiro pelo segundo
-
-Se a opção digitada for inválida, mostrar uma mensagem de erro e terminar a execução do programa.
-*/

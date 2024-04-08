32a943cc81617d6fffb57266b1e84e76f3757d8c
João Souto <joaosouto0@outlook.com>
2024-04-08 09:23:33 -0300

    update

--- exerciciosWhile/while4.java
+++ exerciciosWhile/while4.java
@@ -6,8 +6,20 @@ public class while4 {
 
         System.out.println("Insira a quantidade de alunos desta sala: ");
         int quantidadeAlunos = leia.nextInt();
-        int i =1;
+        int i = 1;
+        float soma = 0;
+        float notas;
 
+        while (i<=quantidadeAlunos) {
+            System.out.println("Insira a nota do aluno " + i + ": ");
+            notas = leia.nextFloat();
+            soma = soma + notas;
+            i++;
+        }
+
+        System.out.println("A média desta sala é: " + (soma/quantidadeAlunos) + ".");
+
+        leia.close();
     }
 }
 

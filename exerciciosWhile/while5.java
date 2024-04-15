6c801381a2c25548a0f0a8b7c65d9affa3972afd
João Souto <joaosouto0@outlook.com>
2024-04-15 08:42:15 -0300

    Aprendendo a orgnizar métodos

--- exerciciosWhile/while5.java
+++ exerciciosWhile/while5.java
@@ -1,7 +1,37 @@
 package exerciciosWhile;
+import java.util.Scanner;
 
 public class while5 {
-    
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int i = 0;
+        float numeros[] = new float[10];
+
+        while (i<numeros.length) {
+        System.out.println("Insira o valor do número " + (i+1) + ": ");
+        numeros[i] = (float) leia.nextFloat();
+        i++;
+        }
+
+        leia.close();
+
+        media(numeros);
+    }
+
+    public static float media(float[] numeros) {
+                int i = 0;
+                float soma = 0;
+        
+                while (i<numeros.length) {
+                    soma = soma + numeros[i];
+                    i++;
+                }
+
+                float media = soma/10;
+                System.out.println("A média destes números é " + (soma/10) + ".");
+                
+                return media;
+    }
 }
 
 // 5.	Achando o maior número

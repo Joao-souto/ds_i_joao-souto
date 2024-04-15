18a0dcba094cfb021fa89bc44065ddcdbf060057
João Souto <joaosouto0@outlook.com>
2024-04-15 08:55:12 -0300

    Aprendendo a orgnizar métodos

--- exerciciosWhile/while5.java
+++ exerciciosWhile/while5.java
@@ -1,5 +1,7 @@
 package exerciciosWhile;
+
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class while5 {
     public static void main(String[] args) {
@@ -7,36 +9,53 @@ public class while5 {
         int i = 0;
         float numeros[] = new float[10];
 
-        while (i<numeros.length) {
-        System.out.println("Insira o valor do número " + (i+1) + ": ");
-        numeros[i] = (float) leia.nextFloat();
-        i++;
+        while (i < numeros.length) {
+            System.out.println("Insira o valor do número " + (i + 1) + ": ");
+            numeros[i] = (float) leia.nextFloat();
+            i++;
         }
 
-        leia.close();
+        float media = media(numeros);
+        float maior = maior(numeros);
+        float menor = menor(numeros);
 
-        media(numeros);
+        System.out.println("A média destes números é " + media + ".");
+        System.out.println("O maior número entre estes números é " + maior + ".");
+        System.out.println("O menor destes números é " + menor + ".");
+        System.out.println("Sequência organizada: " + Arrays.toString(numeros));
+
+        leia.close();
     }
 
     public static float media(float[] numeros) {
-                int i = 0;
-                float soma = 0;
-        
-                while (i<numeros.length) {
-                    soma = soma + numeros[i];
-                    i++;
-                }
+        int i = 0;
+        float soma = 0;
 
-                float media = soma/10;
-                System.out.println("A média destes números é " + (soma/10) + ".");
-                
-                return media;
+        while (i < numeros.length) {
+            soma = soma + numeros[i];
+            i++;
+        }
+        float media = soma / 10;
+        return media;
+    }
+
+    public static float maior(float[] numeros) {
+        float maior = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+        }
+        return maior;
+    }
+
+    public static float menor(float[] numeros) {
+        float menor = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+        return menor;
     }
 }
-
-// 5.	Achando o maior número
-// Achar o maior, menor, média e organizar números ou sequências são os algoritmos mais importantes e estudados em Computação. Em Java não poderia ser diferente.
-// Em nosso curso, obviamente, também não será diferente.
-
-// Escreva um programa em Java que solicita 10 números ao usuário, através de um laço while, e ao final
-// mostre qual destes números é o maior.
